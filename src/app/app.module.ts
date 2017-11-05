9af4d934312e7df60e9bbecb353607ae1bf0715a
juan <juanda4370@gmail.com>
2017-11-04 21:22:48 -0500

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ClienteModule} from './cliente/cliente.module';
+import { ProductoModule} from './producto/producto.module';
+import { ProveedorModule} from './proveedor/proveedor.module';
+
+import {RouterModule,Routes}from '@angular/router';
+
+import { IndexComponent} from './cliente/index/index.component';
+import { IndexComponent as productoaddcomponent} from './producto/index/index.component';
+import { IndexComponent as proveedoraddcomponent} from './proveedor/index/index.component';
+
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  {path:  'cliente/index',component: IndexComponent},
+  {path:  'producto/index',component: productoaddcomponent},
+  {path:  'proveedor/index',component: proveedoraddcomponent},
+ 
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClienteModule,
+    ProductoModule,
+    ProveedorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/current-option/current-option.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrentOptionComponent } from './current-option.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CurrentOptionComponent]
+})
+export class CurrentOptionModule { }
